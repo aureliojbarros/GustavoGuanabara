f09d201c7233474e8bff8fa089c00f3f6bed04aa
Aurelio Jara Barros <102596177+aureliojbarros@users.noreply.github.com>
2024-01-30 19:38:01 -0300

    Update ex036.py

--- Aula12/ex036.py
+++ Aula12/ex036.py
@@ -1,2 +1,11 @@
-#Teste
+#Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. 
+#Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. 
+#A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+
+valorImovel = float(input('Qual o valor do imóvel? '))
+salario = float(input('Qual o valor do salário? '))
+anos = int(input('Em quantos anos o financiamento do imóvel? '))
+
+prestacao = casa / (anos * 12)
+minimo = salario * 30/100
 
