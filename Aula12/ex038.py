eedceebc15ca13ea63653eca5871679215b79cdc
Aurelio Jara Barros <102596177+aureliojbarros@users.noreply.github.com>
2024-02-01 11:51:11 -0300

    Update ex038.py

--- Aula12/ex038.py
+++ Aula12/ex038.py
@@ -2,3 +2,13 @@
 # – O primeiro valor é maior
 # – O segundo valor é maior
 # – Não existe valor maior, os dois são iguais
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print('O primeiro valor {} é o maior'.format(num1))
+elif num2 > num1:
+    print('O segundo valor {} é o maior'.format(num2))
+else:
+    print('Não existe valor maior, os dois são iguais.')
