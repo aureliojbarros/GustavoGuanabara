c905a8802ec7ba56b6cd3282e215ebe34b927a55
Aurelio Jara Barros <102596177+aureliojbarros@users.noreply.github.com>
2024-02-08 20:00:36 -0300

    Delete Aula10 directory

--- Aula10/ex034.py
+++
@@ -1,6 +0,0 @@
-salario = float(input('Digite o valor do salário: '))
-if (salario <= 1.250):
-    novoSalario = (salario + (salario * 10 / 100))
-else:
-    novoSalario = (salario + (salario * 15 / 100))
-    print('Quem ganhava R${:.2f}, passa a ganhar R${:.2f} agora'.format(salario, novoSalario))
