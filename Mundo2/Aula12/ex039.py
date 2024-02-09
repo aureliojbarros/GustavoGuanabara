05609f0271d08eb80a5b2c6fcd23eb85bb536f91
Aurelio Jara Barros <aureliojbarros@hotmail.com>
2024-02-09 18:47:35 -0300

    Update ex039.py

--- Mundo2/Aula12/ex039.py
+++ Mundo2/Aula12/ex039.py
@@ -2,8 +2,12 @@
 #se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
 #Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
+from datetime import date
+atual = date.today().year #Qual o ano atual?
 idade = int(input('Digite sua idade: '))
-
+nascimento = int(input('Digite a data de seu nascimento: '))
+idade = atual - nascimento
+print('Hoje você tem {} anos'.format(idade))
 if idade == 18:
     print('Você está apto para o Serviço Militar.')
 elif idade > 18:
