75bafa1a7e28684822111d5e4a60ac651073bfc4
Aurelio Jara Barros <aureliojbarros@hotmail.com>
2024-02-18 19:42:18 -0300

    Update ex039.py

--- Mundo2/Aula12/ex039.py
+++ Mundo2/Aula12/ex039.py
@@ -10,7 +10,15 @@ idade = atual - nascimento
 print('Hoje você tem {} anos'.format(idade))
 if idade == 18:
     print('Você está apto para o Serviço Militar.')
+elif idade < 18:
+    saldo = idade + 18
+    print('Ainda faltam {} anos para o alistamento.'.format(saldo))
+    ano = atual + saldo
+    print("Seu alistamento será em {}".format(ano))
 elif idade > 18:
-    print('Você já passou no período do alistamento militar!')
+    saldo = 18 - idade
+    print("Você já deveria ter se alistado há {} anos.".format(saldo))
+    ano = atual - saldo
+    print("Seu alistamento foi em {}".format(ano))
 
 
