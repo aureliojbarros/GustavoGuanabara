8f27b493830c32487e9717775f9c0961df0ae024
Aurelio Jara Barros <aureliojbarros@hotmail.com>
2024-02-22 16:09:05 -0300

    Update ex13a.py

---
+++ Mundo2/Aula13/ex13a.py
@@ -0,0 +1,35 @@
+# Exemplos da 1ª aula de Estrutura de Repetições
+
+# # Exibe de 1 à 3
+# print('Exibe de 1 à 3')
+# for c in range (1,4): 
+#     print(c)
+
+# # Exibe de 1 à 6
+# print('Exibe de 1 à 6')
+# for c in range(1,7):
+#     print(c)
+
+# # Exibe de 0 à 6
+# print('Exibe de 1 à 6')
+# for c in range (6, 0, -1):
+#     print(c)
+
+# n = int(input('Digite um número: '))
+# for c in range(0, n+1):
+#     print(c)
+
+# i = int(input('Início: '))
+# p = int(input('Passo: '))
+# f = int(input('Fim: '))
+# for c in range(i, p+1 ,f):
+#     print(c)
+
+# for c in range(0, 3):
+#     n = int(input('Digite um valor: ')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi: {}'.format(s))
