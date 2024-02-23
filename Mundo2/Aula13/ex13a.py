0f0934ddd67ce7ef38d342c69ac8f051104c7711
Aurelio Jara Barros <aureliojbarros@hotmail.com>
2024-02-23 15:42:28 -0300

    Update ex13a.py

--- Mundo2/Aula13/ex13a.py
+++ Mundo2/Aula13/ex13a.py
@@ -10,10 +10,10 @@
 # for c in range(1,7):
 #     print(c)
 
-# # Exibe de 0 à 6
-# print('Exibe de 1 à 6')
-# for c in range (6, 0, -1):
-#     print(c)
+# Exibe de 0 à 6
+print('Exibe de 1 à 6')
+for c in range (6, 0, -1):
+    print(c)
 
 # n = int(input('Digite um número: '))
 # for c in range(0, n+1):
@@ -28,8 +28,8 @@
 # for c in range(0, 3):
 #     n = int(input('Digite um valor: ')
 
-s = 0
-for c in range(0, 4):
-    n = int(input('Digite um valor: '))
-    s += n
-print('O somatório de todos os valores foi: {}'.format(s))
+# s = 0
+# for c in range(0, 4):
+#     n = int(input('Digite um valor: '))
+#     s += n
+# print('O somatório de todos os valores foi: {}'.format(s))
